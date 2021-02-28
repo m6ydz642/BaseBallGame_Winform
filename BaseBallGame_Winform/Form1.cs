98e6f9cf10685ead5919d6c49d6dd2d9efb85895
m6ydz642 <m6ydz642@gmail>
2021-02-28 23:18:26 +0900

    디자인 경고창 등 대략완료

--- BaseBallGame_Winform/Form1.cs
+++ BaseBallGame_Winform/Form1.cs
@@ -13,7 +13,7 @@ namespace BaseBallGame_Winform
     public partial class Form1 : Form
     {
         int _countGame; // 게임카운트
-        bool _statusCheckGame;
+         bool _statusCheckGame; // 게임 상태 활성화 여부
 
         int[] _inputNumberSave // 사용자로부터 입력받은 점수 저장용 (비교할때 꺼내쓸 용도)
         {
@@ -30,15 +30,33 @@ namespace BaseBallGame_Winform
         
         public Form1() // 기본생성자
         {
-            InitializeComponent();
-            _countGame = 0; // 0회차 부터 (게임 시작시 1부터 ++해서 카운트 할거임
-            _statusCheckGame = true;
+          InitializeComponent();
+          _countGame = 0; // 0회차 부터 (게임 시작시 1부터 ++해서 카운트 할거임
+           _statusCheckGame = true;
+
             listViewGameStart.Items.Clear();
             //            textRandomNumber.Text = setRandomNumber().ToString(); // 컴퓨터가 만드는 랜덤함수 생성 호출 
             setRandomNumber();
+            Console.WriteLine("생성자 초기화 값 _countGame : " + _countGame);
+            Console.WriteLine("생성자 초기화 값 _statusCheckGame : " + _statusCheckGame);
         }
 
-       
+   /*     public Form1(int _countGame, bool _statusCheckGame) //인자있는 기본생성자
+        {
+          //  InitializeComponent();
+            MessageBox.Show("인자있는 기본생성자");
+            this._countGame = _countGame; // 0회차 부터 (게임 시작시 1부터 ++해서 카운트 할거임
+            this._statusCheckGame = _statusCheckGame;
+            Console.WriteLine("생성자 초기화 값 _countGame : " + _countGame);
+            Console.WriteLine("생성자 초기화 값 _statusCheckGame : " + _statusCheckGame); 
+             listViewGameStart.Items.Clear();
+            //            textRandomNumber.Text = setRandomNumber().ToString(); // 컴퓨터가 만드는 랜덤함수 생성 호출 
+            setRandomNumber();
+
+        }
+*/
+
+
 
         public int[] setRandomNumber() // 컴퓨터가 생성하는 랜덤함수
         {
@@ -80,22 +98,31 @@ namespace BaseBallGame_Winform
             if (number == null)
                 return;
 
-            // MessageBox.Show("버튼을 클릭했구만! : " + number.Text); 
+            if(_statusCheckGame)
+            {
+               
+
             int lengthNumber = inputNumber.Text.Length;
             if (lengthNumber < 3)
             {
-                _statusCheckGame = false;
+              //  _statusCheckGame = false; // 이새키가 여기 왜있냐 ㅡㅡ;
                 inputNumber.Text += number.Text;// 형변환해서 넣은 값 text로 꺼낸 후 inputNumber에 텍스트로 담음
             }
             else{
                 MessageBox.Show("더 이상 수를 입력할 수 없습니다 3자리까지만 허용됩니다");
             }
 
+            } // 게임상태 체크 if문 + else
+            else
+            {
+                MessageBox.Show("게임이 이미 끝났습니다");
+
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e) // 폼로드
         {
-            
+    
         }
 
         private void buttonClean_Click(object sender, EventArgs e) // 키패드 내용 전체삭제
@@ -129,8 +156,7 @@ namespace BaseBallGame_Winform
         private void buttonRetry_Click(object sender, EventArgs e) // 게임 재시작
         {
 
-            // new Form1(0, true); //  생성자 호출
-            // 생성자로 입력된값 초기값으로 초기화 할려고 했는데 안되네...
+  
             listViewGameStart.Items.Clear();
             listViewGameStart.Items.Add("게임을 다시 시작합니다");
             MessageBox.Show("게임을 처음부터 다시 시작합니다");
@@ -145,24 +171,51 @@ namespace BaseBallGame_Winform
 
         private void buttonStart_Click(object sender, EventArgs e) // 게임시작 버튼
         {
-            if (inputNumber.Text.Length >= 3)// 3자리인지 자리수 검사
+      
+            if (checkNumberLength()  && _countGame < 9 && _statusCheckGame) 
+                // 자릿수가 맞고 게임이 시작중이고 회차가 9회미만이라면
             {
+
                 _countGame++; // 게임 회차 카운트
                 if (_countGame <= 1) // 1회차에만 문구 나오게
                     listViewGameStart.Items.Add("게임을 시작합니다");
 
-
-         
-
                 listViewGameStart.Items.Add(_countGame + "회차 " + inputNumber.Text);
-                inputNumber.Text = ""; // 키패드 값 삭제
-            }
-            else
-            {
-                MessageBox.Show("3자리를 입력해주세요");
+               // inputNumber.Text = ""; // 키패드 값 삭제
 
             }
-            
+
+            if (_countGame >= 9)
+            // 9회차이상이고 게임 _statusCheckGame false처리하면 아래 조건문 조건에 안맞아서 안들어감
+            {
+                MessageBox.Show("게임이 끝났습니다");
+
+                // 여기에 점수 계산 결과 기록하면 됨
+
+                _statusCheckGame = false;
+            }
+
+
+        }
+
+        private bool checkNumberLength() // 자리수 체크 함수
+        {
+            bool checkNumber = false;
+            if (_statusCheckGame) // 게임상태가 활성화 일때만
+            {
+                if (inputNumber.Text.Length >= 3)// 3자리이면서 게임이 끝이 아니면
+                {
+                    checkNumber = true;
+
+                }
+                else
+                {
+                    MessageBox.Show("3자리를 입력해주세요");
+
+                }
+            }
+
+            return checkNumber;
         }
     }
 }
--- BaseBallGame_Winform/Program.cs
+++ BaseBallGame_Winform/Program.cs
@@ -17,6 +17,7 @@ namespace BaseBallGame_Winform
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+          //  Application.Run(new Form1(0,true));
         }
     }
 }
