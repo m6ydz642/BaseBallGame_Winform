d228e08a55ca97561e8cc4a0bef7b8a095b653de
m6ydz642 <m6ydz642@gmail>
2021-03-01 22:35:54 +0900

    단위테스트 프로젝트 생성

---
+++ NUnitTestProject_BaseBallGame_Winform/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace NUnitTestProject_BaseBallGame_Winform
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
